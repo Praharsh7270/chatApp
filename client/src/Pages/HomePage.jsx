fdf28c12eef26c9755e6edf2ca7b973ec24d279c
Praharsh <praharsh_bc22070@srmcem.ac.in>
2025-07-15 17:41:37 +0530

    update

--- client/src/Pages/HomePage.jsx
+++ client/src/Pages/HomePage.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import SideBar from '../components/SideBar'
+import ChatContainer from '../components/ChatContainer'
+import RightSideBar from '../components/RightSideBar'
 
 const HomePage = () => {
+
+    const [selectedUser, setSelectedUser] = React.useState("");
+
   return (
-    <div>HomePage</div>
+    <div className='border w-full h-screen sm:px-[15%] sm:py-[5%]'>
+        <div className={`backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-[100%] grid grid-cols-1 relative ${selectedUser ? 'grid-cols-3' : 'grid-cols-1 sm:grid-cols-2'}`}>
+            <SideBar />
+            <ChatContainer />
+            <RightSideBar />
+        </div>
+    </div>
   )
 }
 
---
+++ client/src/components/SideBar.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import assets from '../assets/assets'
+import { useNavigate } from 'react-router-dom'
+
+const SideBar = ({selectedUser, setSelectedUser}) => {
+
+    const navigate = useNavigate();
+
+  return (
+    <div>
+        <div className='pb-5 '>
+            <div className='flex justify-between items-center '>
+                <img src={assets.logo} alt="logo"  className='max-w-40'/>
+                <div className='relative py-2 group'>
+                    <img src={assets.menu_icon} alt="" className='max-h-5 cursor-pointer'/>
+
+                    <div>
+                        <p onClick={() =>{
+                            navigate('/profile')
+                        }}>Edit Profile</p>
+                        <hr className='my-2 border-t border-gray-500'/>
+                        <p className='cursor-pointer text-sm'>Logout</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default SideBar
