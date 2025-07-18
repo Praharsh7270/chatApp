886d51a6678067629e6aafdc312552ad67efd1f8
Praharsh <praharsh_bc22070@srmcem.ac.in>
2025-07-18 14:05:35 +0530

    update

--- client/src/Pages/HomePage.jsx
+++ client/src/Pages/HomePage.jsx
@@ -10,9 +10,9 @@ const HomePage = () => {
   return (
     <div className='border w-full h-screen sm:px-[15%] sm:py-[5%]'>
         <div className={`backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-[100%] grid grid-cols-1 relative ${selectedUser ? 'grid-cols-3' : 'grid-cols-1 sm:grid-cols-2'}`}>
-            <SideBar />
-            <ChatContainer />
-            <RightSideBar />
+            <SideBar selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
+            <ChatContainer selectedUser={selectedUser} setSelectedUser={setSelectedUser}  />
+            <RightSideBar selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
         </div>
     </div>
   )
--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import assets from '../assets/assets'
 
 const ChatContainer = () => {
   return (
-    <div>ChatContainer</div>
+    <div>
+      <div className='flex items-center gap-3 py-3 mx-4 border-b border-stone-500'>
+        <img src={assets.profile_martin} alt="" className='w-8 rounded-full'/>
+        <p className='flex-1 text-lg text-white flex item-center'>Martin jonson <span className='w-2 h-2 rounded-full bg-green-500'></span></p>
+        <img src={assets.arrow_icon} alt="" className='md:hidden max-w-7' onClick={() =>{
+          // Logic to handle back navigation or collapse chat
+
+          
+        }}/>
+      </div>
+    </div>
   )
 }
 
